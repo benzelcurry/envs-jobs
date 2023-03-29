6f80e077d9f9f9a72753a4bcb4684644629cb863
Ben <benawhiting@gmail.com>
2023-03-29 15:42:56 -0700

    Make sidebar toggle when clicked

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,28 @@ import Sidebar from './Sidebar';
 import './App.css'
 
 function App() {
+  const [view, setView] = useState('');
+
+  useEffect(() => {
+    const viewMode = localStorage.getItem('view');
+    if (viewMode) {
+      setView(viewMode);
+    }
+  }, [view])
+
+  const toggleView = () => {
+    if (!view) {
+      localStorage.setItem('view', 'light')
+      setView('dark');
+    } else {
+      localStorage.removeItem('view');
+      setView('');
+    };
+  };
+
   return (
-    <div className='m-0'>
-      <Sidebar />
+    <div className={!view ? 'dark' : ''}>
+      <Sidebar toggle={toggleView} />
     </div>
   )
 }
