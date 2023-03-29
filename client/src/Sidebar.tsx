79b8b15b5010fe8d47c752b1b753fd24258961d4
Ben <benawhiting@gmail.com>
2023-03-29 15:21:13 -0700

    Access localStorage for light mode toggling

--- client/src/Sidebar.tsx
+++ client/src/Sidebar.tsx
@@ -5,18 +5,30 @@ import { BsFillMoonStarsFill } from 'react-icons/bs';
 import { RiSuitcaseLine } from 'react-icons/ri';
 import { RxAvatar } from 'react-icons/rx';
 
-type Props = {
+type Props = React.HTMLAttributes<HTMLDivElement> & {
   icon: JSX.Element,
   text?: string,
-  dark?: boolean
+  dark?: boolean,
+  onClick?: {}
 };
 
 const Sidebar = () => {
-  const [view, setView] = useState(null);
+  const [view, setView] = useState('');
 
   useEffect(() => {
-    
-  })
+    const viewValue = localStorage.getItem('view');
+    if (viewValue) {
+      setView(viewValue);
+    };
+  }, [view])
+
+  const toggleView = () => {
+    if (view === '') {
+      localStorage.setItem('view', 'light')
+    } else {
+      localStorage.removeItem('view');
+    };
+  };
 
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col 
@@ -24,14 +36,18 @@ const Sidebar = () => {
       <SidebarIcon icon={<AiFillHome size='28' />} text='Home' />
       <SidebarIcon icon={<RiSuitcaseLine size='28' />} text='Careers' />
       <SidebarIcon icon={<RxAvatar size='28' />} text='Profile' />
-      <SidebarIcon icon={<BsFillMoonStarsFill size='28' />} text='Dark Mode' dark={true} />
+      <SidebarIcon icon={<BsFillMoonStarsFill size='28' />} text='Dark Mode' dark={true} 
+        onClick={() => toggleView()}
+      />
     </div>
   );
 };
 
-const SidebarIcon = ({ icon, text = 'tooltip', dark = false }: Props) => {
+const SidebarIcon = ({ icon, text = 'tooltip', dark = false, onClick }: Props) => {
   return (
-    <div className={ dark ? "sidebar-icon group dark" : "sidebar-icon group"}>
+    <div 
+      className={ dark ? "sidebar-icon group dark" : "sidebar-icon group"}
+      onClick={onClick}>
       {icon}
       <span className="sidebar-tooltip group-hover:scale-100">
         {text}
