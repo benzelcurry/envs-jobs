cb051c8ae33289bd66ad5eccd30009e4ed5652e9
Ben <benawhiting@gmail.com>
2023-03-29 13:15:28 -0700

    Create sidebar with basic styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
+
+import Sidebar from './Sidebar';
 import './App.css'
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline text-center text-green-500">
-      Hello, world!
+    <div className='m-0'>
+      <Sidebar />
     </div>
   )
 }
