4ae7fc823e5523ea44856757cc12acdf20344efc
Ben <benawhiting@gmail.com>
2023-03-29 14:12:58 -0700

    Add animations to sidebar

--- client/src/Sidebar.tsx
+++ client/src/Sidebar.tsx
@@ -3,24 +3,28 @@ import { RiSuitcaseLine } from 'react-icons/ri';
 import { RxAvatar } from 'react-icons/rx';
 
 type Props = {
-  icon: JSX.Element;
+  icon: JSX.Element,
+  text?: string,
 };
 
 const Sidebar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col 
                   bg-gray-900 text-white shadow-lg">
-      <SidebarIcon icon={<AiFillHome size='28' />} />
-      <SidebarIcon icon={<RiSuitcaseLine size='28' />} />
-      <SidebarIcon icon={<RxAvatar size='28' />} />
+      <SidebarIcon icon={<AiFillHome size='28' />} text='Home' />
+      <SidebarIcon icon={<RiSuitcaseLine size='28' />} text='Careers' />
+      <SidebarIcon icon={<RxAvatar size='28' />} text='Profile' />
     </div>
   );
 };
 
-const SidebarIcon = ({ icon }: Props) => {
+const SidebarIcon = ({ icon, text = 'tooltip' }: Props) => {
   return (
-    <div className="sidebar-icon">
+    <div className="sidebar-icon group">
       {icon}
+      <span className="sidebar-tooltip group-hover:scale-100">
+        {text}
+      </span>
     </div>
   );
 };
