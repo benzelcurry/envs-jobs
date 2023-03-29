21e8d432b37769268aac1f37b5261923ffe1773a
Ben <benawhiting@gmail.com>
2023-03-29 13:51:39 -0700

    Import icons for sidebar

--- client/src/Sidebar.tsx
+++ client/src/Sidebar.tsx
@@ -1,12 +1,26 @@
+import { AiFillHome } from 'react-icons/ai';
+import { RiSuitcaseLine } from 'react-icons/ri';
+import { RxAvatar } from 'react-icons/rx';
+
+type Props = {
+  icon: JSX.Element;
+};
+
 const Sidebar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col 
                   bg-gray-900 text-white shadow-lg">
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
+      <SidebarIcon icon={<AiFillHome size='28' />} />
+      <SidebarIcon icon={<RiSuitcaseLine size='28' />} />
+      <SidebarIcon icon={<RxAvatar size='28' />} />
+    </div>
+  );
+};
+
+const SidebarIcon = ({ icon }: Props) => {
+  return (
+    <div className="sidebar-icon">
+      {icon}
     </div>
   );
 };
