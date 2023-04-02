a19a3ddf4f33b412cfcc24944a956ef381f7a70d
Ben <benawhiting@gmail.com>
2023-04-02 14:01:25 -0700

    Create basic route handler for user data

---
+++ server/src/routes/users.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response, NextFunction } from 'express';
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.json({
+    msg: 'This is where your users will be returned from!'
+  });
+});
+
+module.exports = router;
