25f85f46041dfb1fcbbf9fd408b86e94b3343786
Ben <benawhiting@gmail.com>
2023-04-02 16:47:58 -0700

    Add a function for creating new users

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
 const router = express.Router();
 
-import {
-  user_list,
-} from '../controllers/userController';
+import { user_list, create_user } from '../controllers/userController';
 
+// Returns list of users; PROBABLY SHOULD DELETE THIS ROUTE, POINTLESS
 router.get('/', user_list);
+
+// Creates a new user on POST
+router.post('/', create_user);
 
 export default router;
