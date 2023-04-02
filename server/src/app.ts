dace538844a6721f819c0db74e2eb0541606f845
Ben <benawhiting@gmail.com>
2023-04-02 14:01:25 -0700

    Create basic route handler for user data

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,11 +14,14 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 require('dotenv').config();
 
 const app: Application = express();
+const users = require('./routes/users');
+
+app.use('/users', users);
 
 app.use('/', (req: Request, res: Response): void => {
   res.json('Hello, World!');
 });
 
 app.listen(process.env.PORT, (): void => {
-  console.log(`Server is running on Port: ${process.env.PORT}`);
+  console.log(`Server is running on Port ${process.env.PORT}`);
 });
