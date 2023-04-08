79a64dcd33e797a1f759a5876caead76dcad18c1
Ben <benawhiting@gmail.com>
2023-04-08 12:57:41 -0700

    Make navbar link to careers page

--- client/src/RouteSwitch.tsx
+++ client/src/RouteSwitch.tsx
@@ -4,12 +4,14 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
+import CareersList from './CareersList';
 
 const RouteSwitch = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path='/careers' element={<CareersList />} />
       </Routes>
     </HashRouter>
   );
