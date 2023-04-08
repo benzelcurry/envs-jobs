7371f6c5ce9bc422bc6c13d17438ea0d3455cb25
Ben <benawhiting@gmail.com>
2023-04-08 13:22:34 -0700

    Create login component and route

---
+++ client/src/LogIn.tsx
@@ -0,0 +1,18 @@
+// Log-in component
+
+import Sidebar from './Sidebar';
+
+const LogIn = () => {
+  return (
+    <div>
+      <Sidebar />
+      <div>
+        <p>
+          Log In logic will go here.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
--- client/src/RouteSwitch.tsx
+++ client/src/RouteSwitch.tsx
@@ -6,6 +6,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import CareersList from './CareersList';
 import Profile from './Profile';
+import LogIn from './LogIn';
 
 const RouteSwitch = () => {
   return (
@@ -14,6 +15,7 @@ const RouteSwitch = () => {
         <Route path="/" element={<App />} />
         <Route path='/careers' element={<CareersList />} />
         <Route path='/profile/:username' element={<Profile />} />
+        <Route path='/log-in' element={<LogIn />} />
       </Routes>
     </HashRouter>
   );
