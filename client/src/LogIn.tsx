5ff35bc99aac6b8ad0e1db31c5c417f54939e0ea
Ben <benawhiting@gmail.com>
2023-04-08 14:39:31 -0700

    Style log-in form

--- client/src/LogIn.tsx
+++ client/src/LogIn.tsx
@@ -26,14 +26,25 @@ const LogIn = () => {
   return (
     <div>
       <Sidebar />
-      <div>
-        <form>
-          <label>Username</label>
-          <input type="text" onChange={(e) => handleUsername(e)} />
-
-          <label>Password</label>
-          <input type="password" onChange={(e) => handlePassword(e)} />
-          <button>Submit</button>
+      <div className="flex flex-col flex-1 mt-10">
+        <form className="flex flex-col items-center gap-7">
+          <div className="grid grid-cols-[100px_auto] gap-5 items-center">
+            <label className='justify-self-end'>Username</label>
+            <input
+              type="text"
+              onChange={(e) => handleUsername(e)}
+              autoComplete="username"
+              className='form-input'
+            />
+            <label className='justify-self-end'>Password</label>
+            <input
+              type="password"
+              onChange={(e) => handlePassword(e)}
+              autoComplete="current-password"
+              className='form-input'
+            />
+          </div>
+          <button className='btn'>Submit</button>
         </form>
       </div>
     </div>
