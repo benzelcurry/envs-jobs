fcc2fc4d5b0cbe59e4d6f6ae335651440e8cb2c6
Ben <benawhiting@gmail.com>
2023-04-08 12:54:15 -0700

    Add skeleton CareersList component

--- client/src/CareersList.tsx
+++ client/src/CareersList.tsx
@@ -7,6 +7,20 @@ const CareersList = () => {
   return (
     <div>
       <Sidebar />
+      <p>
+        Welcome to the careers page, where you can find a list of ENVS careers!
+      </p>
+      <p>
+        Please note, this is not an exhaustive list of{' '}
+        <span className="font-bold italic">every</span> ENVS career in
+        existence. This site may be updated in the future to include more
+        careers.
+      </p>
+      <div>
+        <p>Careers will go here in a grid format.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default CareersList;
