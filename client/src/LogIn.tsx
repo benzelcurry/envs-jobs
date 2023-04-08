382127ddeed8377e5cc4b3213084c31d61c99e7d
Ben <benawhiting@gmail.com>
2023-04-08 13:54:16 -0700

    Add input handlers to form fields for login

--- client/src/LogIn.tsx
+++ client/src/LogIn.tsx
@@ -1,13 +1,40 @@
 // Log-in component for existing users
+import { useState, ChangeEvent } from 'react';
 
 import Sidebar from './Sidebar';
 
 const LogIn = () => {
+  const [body, setBody] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    setBody({
+      ...body,
+      username: e.target.value
+    });
+  };
+
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setBody({
+      ...body,
+      password: e.target.value
+    });
+  };
+
   return (
     <div>
       <Sidebar />
       <div>
-        <p>Log In logic will go here.</p>
+        <form>
+          <label>Username</label>
+          <input type="text" onChange={(e) => handleUsername(e)} />
+
+          <label>Password</label>
+          <input type="password" onChange={(e) => handlePassword(e)} />
+          <button>Submit</button>
+        </form>
       </div>
     </div>
   );
