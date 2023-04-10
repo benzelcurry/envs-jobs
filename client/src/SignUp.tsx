373a8a80ed57fffae48a1068be969dc8c84f5942
Ben <benawhiting@gmail.com>
2023-04-10 14:08:30 -0700

    Add form inputs and improve a11y

--- client/src/SignUp.tsx
+++ client/src/SignUp.tsx
@@ -34,31 +34,62 @@ const SignUp = () => {
       default:
         console.log('Not sure how you ended up here.');
         break;
-    };
+    }
   };
 
   return (
     <div>
       <Sidebar />
       <div className="flex flex-col flex-1 mt-10 items-center">
-        <form className="flex flex-col items-center gap-7" onChange={(e) => handleInput(e)}>
-          <div className="grid grid-cols-[100px_auto] gap-5 items-center">
+        <form
+          className="flex flex-col items-center gap-7"
+          onChange={(e) => handleInput(e)}
+        >
+          <div className="grid grid-cols-[150px_auto] gap-5 items-center">
+            <label className="justify-self-end">First Name</label>
+            <input
+              aria-label="First name"
+              type="text"
+              id="first_name"
+              autoComplete="given-name"
+              className="form-input"
+            />
+            <label className="justify-self-end">Family Name</label>
+            <input
+              aria-label="Family name"
+              type="text"
+              id="family_name"
+              autoComplete="family-name"
+              className="form-input"
+            />
             <label className="justify-self-end">Username</label>
             <input
+              aria-label="Username"
               type="text"
-              id='username'
+              id="username"
               autoComplete="username"
               className="form-input"
             />
             <label className="justify-self-end">Password</label>
             <input
+              aria-label="Password"
               type="password"
-              id='password'
-              autoComplete="current-password"
+              id="password"
+              autoComplete="new-password"
+              className="form-input"
+            />
+            <label className="justify-self-end">Confirm Password</label>
+            <input
+              aria-label="Confirm password"
+              type="password"
+              id="confirm_password"
+              autoComplete="new-password"
               className="form-input"
             />
           </div>
-          <button className="btn">Create Account</button>
+          <button aria-label="Create account" className="btn">
+            Create Account
+          </button>
         </form>
       </div>
     </div>
