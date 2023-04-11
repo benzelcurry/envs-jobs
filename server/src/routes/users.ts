3d0203d6891e7591ccfd67af18ae161333b5321d
benzelcurry <benawhiting@gmail.com>
2023-04-11 11:56:40 -0700

    Add route for logging user in

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express';
 const router = express.Router();
 
-import { user_list, create_user } from '../controllers/userController';
+import { user_list, create_user, login_user } from '../controllers/userController';
 
 // Returns list of users; PROBABLY SHOULD DELETE THIS ROUTE, POINTLESS
 router.get('/', user_list);
 
 // Creates a new user on POST
 router.post('/', create_user);
+
+// Logs user in on POST
+router.post('/login', login_user);
 
 export default router;
