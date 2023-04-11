c12445f2e33503f1f7041ea77042db8b36946697
benzelcurry <benawhiting@gmail.com>
2023-04-11 13:34:54 -0700

    Give log-in form functionality and error handling

--- client/src/LogIn.tsx
+++ client/src/LogIn.tsx
@@ -1,16 +1,18 @@
 // Log-in component for existing users
-import { useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import Sidebar from './Sidebar';
-// TODO:
-//   1. Implement log-in (use /api/... ; check vite.config.ts)
 
 const LogIn = () => {
+  const [error, setError] = useState('');
   const [body, setBody] = useState({
     username: '',
     password: ''
   });
+
+  const navigate = useNavigate();
 
   const handleUsername = (e: ChangeEvent<HTMLInputElement>) => {
     setBody({
@@ -26,11 +28,28 @@ const LogIn = () => {
     });
   };
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    axios
+      .post('/api/users/login', body)
+      .then((response) => {
+        if (response.data.message === 'Successful') {
+          window.localStorage.setItem('token', response.data.token);
+          navigate('/');
+        } else {
+          setError(response.data.errors);
+        };
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+
   return (
     <div>
       <Sidebar />
       <div className="flex flex-col flex-1 mt-10 items-center">
-        <form className="flex flex-col items-center gap-7">
+        <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col items-center gap-7">
           <div className="grid grid-cols-[100px_auto] gap-5 items-center">
             <label className="justify-self-end">Username</label>
             <input
@@ -52,6 +71,9 @@ const LogIn = () => {
           <button aria-label="Log in" className="btn">
             Log In
           </button>
+          {
+           error ? <p className="text-red-500">{error}</p> : null
+          }
         </form>
         <p className="mt-10">
           New user?{' '}
