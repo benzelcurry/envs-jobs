42b9171f2941dcd7a4d5dddf97b342e7f176f68b
benzelcurry <benawhiting@gmail.com>
2023-04-12 13:50:57 -0700

    Make separate file for defining types and interfaces

--- client/src/RouteSwitch.tsx
+++ client/src/RouteSwitch.tsx
@@ -1,7 +1,8 @@
 // Router component
 // Using hash router because some hosting services struggle with default router
-
+import { useState, useEffect } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 import Home from './Home';
 import CareersList from './CareersList';
@@ -9,11 +10,36 @@ import Profile from './Profile';
 import LogIn from './LogIn';
 import SignUp from './SignUp';
 
+import { User } from './types';
+
 const RouteSwitch = () => {
+  const [currentUser, setCurrentUser] = useState<User>({
+    username: '',
+    first_name: '',
+    family_name: '',
+    is_admin: false,
+    attributes: []
+  });
+
+  // Gets active user if one is logged in
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      axios
+        .post('/api/users/info', { token: token })
+        .then((response) => {
+          setCurrentUser(response.data)
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
+    };
+  }, []);
+
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home user={currentUser} />} />
         <Route path="/careers" element={<CareersList />} />
         <Route path="/profile/:username" element={<Profile />} />
         <Route path="/log-in" element={<LogIn />} />
---
+++ client/src/types.d.ts
@@ -0,0 +1,7 @@
+export interface User {
+  username: string;
+  first_name: string;
+  family_name: string;
+  is_admin: boolean;
+  attributes: string[];
+}
