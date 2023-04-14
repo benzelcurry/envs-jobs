92491a9b40b0ec8253ca1bc6dd206cf75d4b5214
Ben <benawhiting@gmail.com>
2023-04-14 12:21:01 -0700

    Provide basic styling to modification components

--- client/src/components/ModifyCareers.tsx
+++ client/src/components/ModifyCareers.tsx
@@ -1,7 +1,16 @@
 // Admin-restricted page for modifying careers
 
+import Sidebar from './Sidebar';
+
 const ModifyCareers = () => {
-  return <></>;
+  return (
+    <div>
+      <Sidebar />
+      <div className="flex flex-col flex-1 mt-10 p-10">
+        Contents will go here.
+      </div>
+    </div>
+  );
 };
 
 export default ModifyCareers;
--- client/src/components/ModifyQuestions.tsx
+++ client/src/components/ModifyQuestions.tsx
@@ -1,7 +1,16 @@
 // Admin-restricted page for modifying questionnaire
 
+import Sidebar from './Sidebar';
+
 const ModifyQuestions = () => {
-  return <></>;
+  return (
+    <div>
+      <Sidebar />
+      <div className="flex flex-col flex-1 mt-10 p-10">
+        Contents will go here.
+      </div>
+    </div>
+  );
 };
 
 export default ModifyQuestions;
