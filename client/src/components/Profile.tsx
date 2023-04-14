aecf8dcb48c244d39f0f7ea9ca4a7591b651fc80
Ben <benawhiting@gmail.com>
2023-04-14 12:11:29 -0700

    Add component and route allowing admins to modify careers

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -2,7 +2,7 @@
 // Displays user info (name, career rankings, etc.)
 
 import Sidebar from './Sidebar';
-import PermissionDenied from './PermissionDenied'; 
+import PermissionDenied from './PermissionDenied';
 
 import { User } from '../types';
 
@@ -13,19 +13,22 @@ const Profile = ({ user }: { user: User }) => {
   return (
     <div>
       <Sidebar />
-      { user.username ?
-      <div className="flex flex-col flex-1 mt-10 p-10">
-        <h1 className='text-5xl mb-5'>{user.first_name} {user.family_name}</h1>
-        {/* MAKE THESE CONDITIONALLY DISPLAY DEPENDING ON USER STATUS */}
-        <div className='p-2'>
-          <h3>Admin contents.</h3>
+      {user.username ? (
+        <div className="flex flex-col flex-1 mt-10 p-10">
+          <h1 className="text-5xl mb-5">
+            {user.first_name} {user.family_name}
+          </h1>
+          {/* MAKE THESE CONDITIONALLY DISPLAY DEPENDING ON USER STATUS */}
+          <div className="p-2">
+            <h3>Admin contents.</h3>
+          </div>
+          <div className="p-2">
+            <h3>Non-admin contents.</h3>
+          </div>
         </div>
-        <div className='p-2'>
-          <h3>Non-admin contents.</h3>
-        </div>
-      </div>
-      : <PermissionDenied />
-      }      
+      ) : (
+        <PermissionDenied />
+      )}
     </div>
   );
 };
