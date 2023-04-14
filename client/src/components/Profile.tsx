9a97e5ac299c7cf445190d1cea6678919f44abf7
Ben <benawhiting@gmail.com>
2023-04-14 16:15:47 -0700

    Optimize profile page styling for mobile devices

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -19,18 +19,31 @@ const Profile = ({ user }: { user: User }) => {
           <h1 className="text-5xl mb-5">
             {user.first_name} {user.family_name}
           </h1>
-          {/* MAKE THESE CONDITIONALLY DISPLAY DEPENDING ON USER STATUS */}
           <div className="p-2">
-            <h3>Admin contents.</h3>
-            <Link to="/modify-questions">
-              <button className="btn">Modify Questions</button>
-            </Link>
-            <Link to="/modify-careers">
-              <button className="btn">Modify Careers</button>
-            </Link>
+            <ul>
+              { 
+                user.attributes.length > 0 ?
+                user.attributes.map((attribute) => 
+                  <li>{attribute}</li>
+                )
+                :
+                <div className='flex flex-col'>
+                  <p>Take the questionnaire below to find out what career attributes you prefer!</p>
+                  <button className="btn mt-3 mb-3 self-center w-[200px] md:self-start">Questionnaire</button>
+                </div>
+              }
+            </ul>
           </div>
           <div className="p-2">
-            <h3>Non-admin contents.</h3>
+            <h3>Admin contents.</h3>
+            <div className='flex justify-center md:justify-start'>
+              <Link to="/modify-questions">
+                <button className="btn m-5 w-[200px]">Modify Questions</button>
+              </Link>
+              <Link to="/modify-careers">
+                <button className="btn m-5 w-[200px]">Modify Careers</button>
+              </Link>
+            </div>
           </div>
         </div>
       ) : (
