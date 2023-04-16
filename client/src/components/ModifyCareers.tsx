55ef987df01f05c677b774bf4e74d42198a8fcda
Ben <benawhiting@gmail.com>
2023-04-16 14:13:55 -0700

    Adjust styling of modify career form

--- client/src/components/ModifyCareers.tsx
+++ client/src/components/ModifyCareers.tsx
@@ -43,23 +43,28 @@ const ModifyCareers = ({ user }: { user: User }) => {
       {user.is_admin ? (
         <div className="flex flex-col flex-1 mt-10 p-10">
           <div>
-            <h2>Current Careers:</h2>
-            {/* <ul className="list-disc mt-2"> */}
+            <h2 className='text-3xl border-b-2 border-green-500 inline-block text-green-500'>Current Careers:</h2>
+            <ul className="list-disc mt-2">
               {careers.map((career) => (
-                <div
+                <li
                   aria-label="Click to modify career details"
                   key={Math.random() * 999}
-                  className="ml-6 cursor-pointer hover:text-green-500 transition-all duration-300"
+                  className={
+                    clickedTitle === career.title ?
+                    "ml-6 border-2 border-rad rounded-md p-4 transition-all duration-300 border-green-500 md:w-[70%]"
+                    :
+                    "ml-6 p-4"
+                  }
                 >
-                  <h3 onClick={() => displayForm(career.title)}>{career.title}</h3>
+                  <h3 className='text-lg cursor-pointer hover:text-green-500 transition-all duration-300 inline-block' onClick={() => displayForm(career.title)}>{career.title}</h3>
                   {
                     clickedTitle === career.title ?
                     <ModificationForm title={career.title} description={career.description} attributes={career.attributes} />
                     : null
                   }
-                </div>
+                </li>
               ))}
-            {/* </ul> */}
+            </ul>
           </div>
         </div>
       ) : (
@@ -78,18 +83,18 @@ const ModificationForm = ({ title, description, attributes }: Career) => {
   };
 
   return (
-    <form className='grid'>
+    <form className='grid grid-cols-[100px_auto] gap-5 mt-6'>
       <label htmlFor='career-title'>Title: </label>
-      <input type="text" id='career-title' name='career-title' defaultValue={title} />
+      <input type="text" id='career-title' name='career-title' defaultValue={title} className='text-black p-2' />
 
       <label htmlFor='career-description'>Description: </label>
-      <textarea id='career-description' name='career-description' defaultValue={description} />
+      <textarea id='career-description' name='career-description' defaultValue={description} className='text-black p-2' />
 
       <label htmlFor='career-attributes'>Attributes: </label>
-      <div>
+      <div className='flex flex-col gap-5'>
         {
           attributes.map((attribute) => 
-            <input key={Math.random() * 999} type="text" defaultValue={attribute} />
+            <input key={Math.random() * 999} type="text" defaultValue={attribute} className='text-black p-2' />
           )
         }
       </div>
