70c18e373f953adc7447c81733afa1bd5a632dba
benzelcurry <benawhiting@gmail.com>
2023-04-18 13:47:34 -0700

    Add method and route for updating career

--- server/src/routes/careers.ts
+++ server/src/routes/careers.ts
@@ -1,7 +1,11 @@
 import express, { RequestHandler } from 'express';
 const router = express.Router();
 
-import { career_list, add_career } from '../controllers/careerController';
+import {
+  career_list,
+  add_career,
+  update_career
+} from '../controllers/careerController';
 
 // Returns list of careers on GET
 router.get('/', career_list);
@@ -9,5 +13,9 @@ router.get('/', career_list);
 // Adds a new career on POST
 // TODO: Restrict to admins; maybe add middleware to route that does this?
 router.post('/', add_career);
+
+// Updates a career on PUT
+// TODO: Restrict to admins; maybe add middleware to route that does this?
+router.put('/', update_career);
 
 export default router;
