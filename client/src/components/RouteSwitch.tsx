dec1015676894ac403a675506645ce6d4c611193
Ben <benawhiting@gmail.com>
2023-04-17 17:59:18 -0700

    Prevent active users from viewing login page

--- client/src/components/RouteSwitch.tsx
+++ client/src/components/RouteSwitch.tsx
@@ -46,7 +46,7 @@ const RouteSwitch = () => {
         <Route path="/" element={<Home user={currentUser} />} />
         <Route path="/careers" element={<CareersList />} />
         <Route path="/profile" element={<Profile user={currentUser} />} />
-        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/log-in" element={<LogIn user={currentUser} />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route
           path="/modify-questions"
