1423928bdea0ff735dfcf7622381bef7310d7496
benzelcurry <benawhiting@gmail.com>
2023-04-25 13:54:06 -0700

    Map career titles to list elements

--- client/src/components/CareersList.tsx
+++ client/src/components/CareersList.tsx
@@ -2,6 +2,7 @@
 // Displays a list of all careers that are on website
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 import Sidebar from './Sidebar';
 import { Career } from '../types';
@@ -10,8 +11,8 @@ const CareersList = () => {
   const [careers, setCareers] = useState<Career[]>();
 
   useEffect(() => {
-    axios 
-      .get(('/api/careers'))
+    axios
+      .get('/api/careers')
       .then((response) => {
         setCareers(response.data);
       })
@@ -24,7 +25,17 @@ const CareersList = () => {
     <div>
       <Sidebar />
       <div className="flex flex-col flex-1 mt-10">
-        
+        <ul className="list-disc mt-2 ml-10">
+          {careers?.map((career) => (
+            <li
+              aria-label="Click to view more details"
+              key={uuidv4()}
+              className="ml-6 p-4"
+            >
+              {career.title}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
