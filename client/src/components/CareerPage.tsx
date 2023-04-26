4d850e8c91ac70d3c1f9d7ab64b56b721b47fa60
benzelcurry <benawhiting@gmail.com>
2023-04-26 14:43:09 -0700

    Add some basic styling to careers page

--- client/src/components/CareerPage.tsx
+++ client/src/components/CareerPage.tsx
@@ -33,21 +33,28 @@ const CareerPage = () => {
   return (
     <div>
       <Sidebar />
-      <div>
-        <p>{career?.title}</p>
-        <p>{career?.description}</p>
-        <ul>
-          {career?.attributes.map((attribute) => (
-            <li key={uuidv4()}>{attribute}</li>
-          ))}
-        </ul>
-        <p>{career?.bio_quote}</p>
-        {career?.bio_photo ? (
-          <img src={`${import.meta.env.VITE_IMAGES}/${career?.bio_photo}`} />
-        ) : null}
+      <div className='flex flex-col items-center mx-auto pt-5 gap-5'>
+        <p className='border-b-2 border-green-500 text-5xl text-green-500'>{career?.title}</p>
         {career?.job_photo ? (
           <img src={`${import.meta.env.VITE_IMAGES}/${career?.job_photo}`} />
         ) : null}
+        <p>{career?.description}</p>
+        <div className='grid grid-cols-2'>
+          <div className="flex flex-col">
+            <h2 className='text-3xl'><span className='border-b-2 pb-2'>Career Attributes</span></h2>
+            <ul className='m-4 list-disc'>
+              {career?.attributes.map((attribute) => (
+                <li key={uuidv4()}>{attribute}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <p>{career?.bio_quote}</p>
+            {career?.bio_photo ? (
+              <img src={`${import.meta.env.VITE_IMAGES}/${career?.bio_photo}`} />
+            ) : null}
+          </div>
+        </div>
       </div>
     </div>
   );
