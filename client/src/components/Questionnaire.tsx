b92277ae93f286279c00837395b944eacf8d7416
benzelcurry <benawhiting@gmail.com>
2023-04-27 14:49:02 -0700

    Add basic notification modal if user has already taken questionnaire

--- client/src/components/Questionnaire.tsx
+++ client/src/components/Questionnaire.tsx
@@ -9,7 +9,17 @@ const Questionnaire = ({ user }: { user: User }) => {
     <div>
       <Sidebar />
       <div className="flex flex-col flex-1 mt-10 p-10">
-        Questionnaire stuff will go here once ready.
+        {
+          user.attributes ?
+          <div className='md:w-[600px] md:h-[200px] gap-5 p-5 bg-gray-500 flex flex-col items-center rounded-xl'>
+            <p>You've already taken the questionnaire.</p>
+            <p>Click below to take it again.</p>
+            <div>
+              <button type='button' className='btn'>Retake Questionnaire</button>
+            </div>
+          </div>
+          : null
+        }
       </div>
     </div>
   );
