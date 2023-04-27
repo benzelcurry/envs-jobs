863b510449202646914fdd907606c13e2dc55d37
benzelcurry <benawhiting@gmail.com>
2023-04-27 15:37:53 -0700

    Move Question logic into its own component

---
+++ client/src/components/Question.tsx
@@ -0,0 +1,50 @@
+// Modal component for displaying Questions
+import { useState, useEffect } from 'react';
+
+import { User } from '../types';
+
+interface QuestionProps {
+  question: string;
+  answerOne: string;
+  answerTwo: string;
+  attributeOne: string;
+  attributeTwo: string;
+  user: User;
+  setAttributes: React.Dispatch<React.SetStateAction<string[] | undefined>>;
+}
+
+// TODO
+//   1. Modify data models to hold answers and corresponding attributes
+//      in either an object or array to easily assign an attribute upon an answer
+//   2. Figure out how to store answers as they're solved and then push all
+//      attributes at once to user... probably will need to send a setAttributes
+//      from <Questionnaire />
+//   3. Improve styling of cards in light mode and mobile
+const Question = ({ props }: { props: QuestionProps }) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+    return () => setShow(false);
+  }, []);
+
+  return (
+    <div
+      className={`md:w-[600px] md:h-[200px] gap-5 p-5 bg-gray-500 flex flex-col items-center rounded-xl transform transition-all duration-500 ease-in-out ${
+        show ? 'opacity-100 translate-x-0' : 'opacity-0'
+      }`}
+    >
+      <p>{props.question}</p>
+      <div>
+        <button type="button" className="btn">
+          {props.answerOne}
+        </button>
+        <button onClick={() => setShow(false)} type="button" className="btn">
+          {props.answerTwo}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
--- client/src/components/Questionnaire.tsx
+++ client/src/components/Questionnaire.tsx
@@ -1,16 +1,13 @@
 // Questionnaire page for determining student attributes
 import { useState, useEffect } from 'react';
+
 import Sidebar from './Sidebar';
+import Question from './Question';
 
 import { User } from '../types';
 
 const Questionnaire = ({ user }: { user: User }) => {
-  const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    setShow(true);
-    return () => setShow(false);
-  }, []);
+  const [attributes, setAttributes] = useState<string[]>();
 
   return (
     <div>
@@ -27,23 +24,32 @@ const Questionnaire = ({ user }: { user: User }) => {
             </div>
           </div>
         ) : (
-          <div
-            className={`md:w-[600px] md:h-[200px] gap-5 p-5 bg-gray-500 flex flex-col items-center rounded-xl transform transition-all duration-500 ease-in-out ${show ? 'opacity-100 translate-x-0' : 'opacity-0'}`}
-          >
-            <p>What is your favorite flavor of ice cream?</p>
-            <div>
-              <button type="button" className="btn">
-                Vanilla
-              </button>
-              <button
-                onClick={() => setShow(false)}
-                type="button"
-                className="btn"
-              >
-                Chocolate
-              </button>
-            </div>
-          </div>
+          /*
+          TODO:
+            1. Map these Questions to a list of questions that get
+               pulled in from the database using a useEffect hook
+            2. Figure out how to toggle the display of the next question 
+               once the previous question has been answered
+             2.1 Move show/setShow to this component and pass to Question?
+              2.1.1 Toggle which one is shown using uuidv4 or something similar?
+            3. Add a new page/component that users are redirected to once
+               they finish the questionnaire
+             3.1 Show their attributes and job matches
+              3.1.1 Let them know they can find this info on their profile
+                    in the future
+              3.1.2 Make jobs link to respective pages
+          */
+          <Question
+            props={{
+              question: 'Hello world',
+              answerOne: 'Vanilla',
+              answerTwo: 'Chocolate',
+              attributeOne: 'lol',
+              attributeTwo: 'rofl',
+              user: user,
+              setAttributes: setAttributes
+            }}
+          />
         )}
       </div>
     </div>
