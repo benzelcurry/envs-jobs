219805de74e76387cfb9971d6d5ecd85ad20d440
Ben <benawhiting@gmail.com>
2023-04-28 15:18:51 -0700

    Improve formatting and responsiveness for displaying attribute table

--- client/src/components/AddQuestions.tsx
+++ client/src/components/AddQuestions.tsx
@@ -9,6 +9,7 @@ import { User, Question } from '../types';
 
 const AddQuestions = ({ user }: { user: User }) => {
   const [questions, setQuestions] = useState<Question[]>();
+  const [showTable, setShowTable] = useState('');
 
   useEffect(() => {
     axios
@@ -21,6 +22,10 @@ const AddQuestions = ({ user }: { user: User }) => {
       });
   }, []);
 
+  const displayTable = (tableID: string) => {
+    showTable === tableID ? setShowTable('') : setShowTable(tableID);
+  };
+
   return (
     <div>
       <Sidebar />
@@ -30,37 +35,51 @@ const AddQuestions = ({ user }: { user: User }) => {
             <h2 className="text-3xl border-b-2 inline-block border-green-500 text-green-500">
               Current Questions
             </h2>
-            <ul className="list-disc mt-2">
-              {
-                questions &&
-                questions.map((question) => 
-                  <>
-                    <li key={question.id} className='ml-6 p-2'>{question.prompt}</li>
-                    <table className="ml-10 table w-full border-collapse border-[2px] border-black dark:border-gray-300">
-                      <thead className="bg-gray-200 text-black">
-                        <tr>
-                          <th className="py-2 px-4">Answer</th>
-                          <th className="py-2 px-4 border-l-[2px] border-black">Attribute</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr className="bg-white">
-                          <td className="py-2 px-4 text-black">{question.answer_one[0]}</td>
-                          <td className="py-2 px-4 text-black border-l-[2px] border-black">{question.answer_one[1]}</td>
-                        </tr>
-                        <tr className="bg-gray-100">
-                          <td className="py-2 px-4 text-black">{question.answer_two[0]}</td>
-                          <td className="py-2 px-4 text-black border-l-[2px] border-black">{question.answer_two[1]}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </>  
-                  
-                )
-              }
-            </ul>
+            <ol className="list-decimal">
+              {questions &&
+                questions.map((question) => (
+                  <li key={question.id} className="ml-6 p-2">
+                    <h3
+                      onClick={() => displayTable(question.id)}
+                      className="cursor-pointer hover:text-green-300 inline-block"
+                    >
+                      {question.prompt}
+                    </h3>
+                    {question.id === showTable && (
+                      <table className="mt-2 table w-full border-collapse border-[2px] border-black dark:border-gray-300">
+                        <thead className="bg-gray-200 text-black">
+                          <tr>
+                            <th className="py-2 px-4">Answer</th>
+                            <th className="py-2 px-4 border-l-[2px] border-black">
+                              Attribute
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr className="bg-white">
+                            <td className="py-2 px-4 text-black">
+                              {question.answer_one[0]}
+                            </td>
+                            <td className="py-2 px-4 text-black border-l-[2px] border-black">
+                              {question.answer_one[1]}
+                            </td>
+                          </tr>
+                          <tr className="bg-gray-100">
+                            <td className="py-2 px-4 text-black">
+                              {question.answer_two[0]}
+                            </td>
+                            <td className="py-2 px-4 text-black border-l-[2px] border-black">
+                              {question.answer_two[1]}
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    )}
+                  </li>
+                ))}
+            </ol>
 
-            <h2 className="text-3xl border-b-2 inline-block border-green-500 text-green-500">
+            <h2 className="mt-4 text-3xl border-b-2 inline-block border-green-500 text-green-500">
               Add New Question
             </h2>
           </div>
