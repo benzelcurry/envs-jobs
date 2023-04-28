ba507dce35cbb2e5f322938cfbdeabf628197c33
Ben <benawhiting@gmail.com>
2023-04-28 14:50:05 -0700

    Display a table of the corresponding answers and attributes for a question

--- client/src/components/AddQuestions.tsx
+++ client/src/components/AddQuestions.tsx
@@ -10,6 +10,17 @@ import { User, Question } from '../types';
 const AddQuestions = ({ user }: { user: User }) => {
   const [questions, setQuestions] = useState<Question[]>();
 
+  useEffect(() => {
+    axios
+      .get('/api/questions')
+      .then((response) => {
+        setQuestions(response.data);
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }, []);
+
   return (
     <div>
       <Sidebar />
@@ -19,7 +30,35 @@ const AddQuestions = ({ user }: { user: User }) => {
             <h2 className="text-3xl border-b-2 inline-block border-green-500 text-green-500">
               Current Questions
             </h2>
-            <ul className="list-disc mt-2"></ul>
+            <ul className="list-disc mt-2">
+              {
+                questions &&
+                questions.map((question) => 
+                  <>
+                    <li key={question.id} className='ml-6 p-2'>{question.prompt}</li>
+                    <table className="ml-10 table w-full border-collapse border-[2px] border-black dark:border-gray-300">
+                      <thead className="bg-gray-200 text-black">
+                        <tr>
+                          <th className="py-2 px-4">Answer</th>
+                          <th className="py-2 px-4 border-l-[2px] border-black">Attribute</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr className="bg-white">
+                          <td className="py-2 px-4 text-black">{question.answer_one[0]}</td>
+                          <td className="py-2 px-4 text-black border-l-[2px] border-black">{question.answer_one[1]}</td>
+                        </tr>
+                        <tr className="bg-gray-100">
+                          <td className="py-2 px-4 text-black">{question.answer_two[0]}</td>
+                          <td className="py-2 px-4 text-black border-l-[2px] border-black">{question.answer_two[1]}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </>  
+                  
+                )
+              }
+            </ul>
 
             <h2 className="text-3xl border-b-2 inline-block border-green-500 text-green-500">
               Add New Question
--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -16,6 +16,7 @@ export interface Career {
 }
 
 export interface Question {
+  id: string;
   prompt: string;
   answer_one: [string, string];
   answer_two: [string, string];
