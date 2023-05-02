861f33571e80886c53b86c76c6f7e749215a8a69
benzelcurry <benawhiting@gmail.com>
2023-05-02 11:39:00 -0700

    Add certifications to Career model

--- server/src/models/Career.ts
+++ server/src/models/Career.ts
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 
 // This schema is subject to change. Potential additions:
 // * Job listings (pulled from LinkedIn or Indeed API if available?)
-// * Helpful certificates/degrees
 // * Pay range (only add if can pull from a 3rd party API that updates; might not be a DB call)
 const CareerSchema = new Schema({
   title: {
@@ -17,6 +16,7 @@ const CareerSchema = new Schema({
   },
   description: { type: String, required: true, minLength: 2 },
   attributes: { type: [String], required: true, minItems: 3 },
+  certifications: { type: [String] },
   job_photo: { type: String },
   bio_photo: { type: String },
   bio_quote: { type: String }
