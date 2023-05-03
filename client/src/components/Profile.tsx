9792547510a86dc456ab2c727c20a1de65ce3bba
benzelcurry <benawhiting@gmail.com>
2023-05-03 14:24:42 -0700

    List careers in order of rank on profile page

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,15 +1,53 @@
 // Profile page component
 // Displays user info (name, career rankings, etc.)
 
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
 import Sidebar from './Sidebar';
 import PermissionDenied from './PermissionDenied';
 
-import { User } from '../types';
+import { User, Career } from '../types';
+
+interface Match {
+  title: string;
+  percentage: number;
+}
 
 const Profile = ({ user }: { user: User }) => {
+  const [careers, setCareers] = useState<Career[]>([]);
+  const [matches, setMatches] = useState<Match[]>([]);
+
+  // Pulls careers from DB and stores in state
+  useEffect(() => {
+    axios
+      .get('/api/careers')
+      .then((response) => {
+        setCareers(response.data);
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }, []);
+
+  // Ranks careers in order of shared attributes with user
+  useEffect(() => {
+    const tempMatches: Match[] = [];
+    for (const job of careers) {
+      let tracker = 0;
+      for (const trait of user.attributes) {
+        if (job.attributes.includes(trait)) tracker++;
+      }
+      tempMatches.push({
+        title: job.title,
+        percentage: Number((tracker / user.attributes.length).toFixed(2))
+      });
+    }
+    setMatches(tempMatches);
+  }, [careers]);
+
   return (
     <div>
       <Sidebar />
@@ -21,17 +59,39 @@ const Profile = ({ user }: { user: User }) => {
           <div className="p-2">
             {user.attributes.length > 0 ? (
               <div>
-                <h3 className="text-xl font-bold border-b-2 border-green-500 inline-block text-green-500">
-                  Career Attributes:{' '}
-                </h3>
-                <ul className="list-disc pt-2">
-                  {user.attributes.map((attribute) => (
-                    <li key={uuidv4()} className="ml-4">
-                      {attribute}
-                    </li>
-                  ))}
-                </ul>
-                {/* ADD CAREER MATCHES HERE */}
+                <div className="flex gap-10">
+                  <div>
+                    <h3 className="text-xl font-bold border-b-2 border-green-500 inline-block text-green-500">
+                      Preferred Attributes:{' '}
+                    </h3>
+                    <ul className="list-disc pt-2">
+                      {user.attributes.map((attribute) => (
+                        <li key={uuidv4()} className="ml-4">
+                          {attribute}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div>
+                    {/* TODO: Make these careers link to their respective pages */}
+                    <h3 className="text-xl font-bold border-b-2 border-green-500 inline-block text-green-500">
+                      Career Matches*:{' '}
+                    </h3>
+                    <ul className="list-disc pt-2">
+                      {matches.map((match) => (
+                        <li key={uuidv4()} className="ml-4">
+                          {match.title}: {match.percentage}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <p className="pt-10 text-sm italic w-[300px] md:w-[600px]">
+                  * Career match scores represent the percentage of your
+                  preferred attributes that appear in the career's listed
+                  attributes. This is a rough estimate of fit and isn't meant to
+                  be a definitive ranking.
+                </p>
               </div>
             ) : (
               <div className="flex flex-col items-center md:items-start">
