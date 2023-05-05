28ef818b804e3e58414a90d1bc65c4333458bac2
Ben <benawhiting@gmail.com>
2023-05-05 15:36:21 -0700

    Modify checkAdmin to look for token in headers as well as body

--- server/src/middleware/checkAdmin.ts
+++ server/src/middleware/checkAdmin.ts
@@ -3,10 +3,11 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 require('dotenv').config();
 
 const checkAdmin: RequestHandler = (req, res, next) => {
-  if (!req.body.token)
+  const token = req.body.token || req.headers.authorization;
+  if (!token)
     return res.status(400).json({ error: 'Path restricted to admins' });
   const decodedToken = jwt.verify(
-    req.body.token,
+    token,
     process.env.SECRET_KEY as string
   ) as JwtPayload;
   if (!decodedToken || !decodedToken.is_admin) {
